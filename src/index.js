d376b2bfd9881eb7d09865aeaee31b0959d4d6c1
christopher <christopher.gauger.pro@gmail.com>
2022-02-21 08:24:57 +0100

    modification chemin du fichier app.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
-import App from "./app.js";
+import App from "./App.js";
 
 ReactDOM.render(
   <React.StrictMode>
